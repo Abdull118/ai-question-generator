f524c4eb63e240d2aa6156df487f855e97844729
Ahmad Naimi <naimi.airsh@gmail.com>
2024-11-10 22:11:38 -0500

    added lecture 14 for CS

--- app/cs/page.js
+++ app/cs/page.js
@@ -147,7 +147,7 @@ const Page = () => {
       {!selectedLecture && (
         <div className={styles.lectureSelection}>
           <h2>Please select a lecture or all:</h2>
-          {['1', '2', '3', '4', '5', '6', '8', '10', '11', '12', '13'].map((lecture) => (
+          {['1', '2', '3', '4', '5', '6', '8', '10', '11', '12', '13', '14'].map((lecture) => (
             <button
               key={lecture}
               className={styles.lectureButton}
