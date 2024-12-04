e76da655e8a5f998e59e50ec3f046b38d330355b
Ahmad Naimi <naimi.airsh@gmail.com>
2024-12-04 03:27:36 -0500

    Updated IP tracker & Added questions for M5 MMI

--- app/page.js
+++ app/page.js
@@ -78,6 +78,17 @@ const Home = () => {
     
   }, []);
 
+  const getIp = async () => {
+    try {
+      const res = await fetch('https://api.ipify.org?format=json');
+      const data = await res.json();
+      return data.ip;
+    } catch (error) {
+      console.error('Error fetching IP address:', error);
+      return 'Unknown IP';
+    }
+  };
+
   return (
     <>
       {showPopup && (
--- app/pmph/page.js
+++ app/pmph/page.js
@@ -88,6 +88,17 @@ const Page = () => {
       storeIpAddressAndLecture();
     }
   }, [selectedLecture]);
+
+  const getIp = async () => {
+    try {
+      const res = await fetch('https://api.ipify.org?format=json');
+      const data = await res.json();
+      return data.ip;
+    } catch (error) {
+      console.error('Error fetching IP address:', error);
+      return 'Unknown IP';
+    }
+  };
   
 
   const handleLectureSelect = (lecture) => {
