a944d5a9365310cb085c515c2e448251c4ca888d
Ahmad Naimi <naimi.airsh@gmail.com>
2025-01-25 17:48:30 -0500

    testing removal of icons

--- app/layout.js
+++ app/layout.js
@@ -18,19 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#ffffff" />
-        {/* Favicon and PWA icons */}
-        <link rel="icon" href="/favicon16x16.ico" type="image/x-icon" sizes="16x16" />
-        <link rel="icon" href="/favicon32.ico" type="image/x-icon" sizes="32x32" />
-        <link rel="icon" href="/favicon64.ico" type="image/x-icon" sizes="64x64" />
-
-        <link rel="icon" href="/favicon192.ico" type="image/x-icon" sizes="192x192" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="256x256" />
-        <link rel="icon" href="/favicon512.ico" type="image/x-icon" sizes="512x512" />
-        <meta name="apple-mobile-web-app-title" content="Next.js PWA" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <link rel="apple-touch-icon" href="/favicon192.ico" />
+ 
       </head>
       <body>{children}</body>
     </html>
